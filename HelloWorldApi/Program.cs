e477984f7c3514f06a3e18d856dee81cb95d1c51
mm-psy <147830298+mm-psy@users.noreply.github.com>
2025-11-25 15:45:10 +0100

    Add Serilog logging configuration and update .gitignore for log files

--- HelloWorldApi/Program.cs
+++ HelloWorldApi/Program.cs
@@ -1,4 +1,17 @@
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json")
+        .Build())
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.File("Logs/log.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -20,3 +33,5 @@ app.UseHttpsRedirection();
 app.MapGet("/hello", () => "Hello World");
 
 app.Run();
+
+Log.CloseAndFlush();
